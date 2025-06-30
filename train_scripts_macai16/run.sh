26371119bdfbbd25d6a56f8798dfda1501005b93
wenjiezhang-z <118110505+wenjiezhang-z@users.noreply.github.com>
2025-07-01 01:55:38 +0800

    Delete train_scripts_macai16 directory

--- train_scripts_macai16/run.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-# Training
-python3 train.py phase --split cuhk42714 --bn_off --backbone convnextv2 --workers 4 --freeze --seq_len 256 --lr 1e-4 --random_seed --trial_name Step1 --cfg configs/m2cai16.yaml
-
-# Evaluation
-python3 save_predictions.py phase --split cuhk42714 --backbone convnextv2 --seq_len 256 --resume ../output/checkpoints/phase/[TRIAL_NAME]/models/checkpoint_best_acc.pth.tar --cfg configs/m2cai16.yaml

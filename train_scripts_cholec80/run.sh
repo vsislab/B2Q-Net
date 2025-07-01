607c0c30e4fd61f6663c1b504421f8c3bb791a90
wenjiezhang-z <118110505+wenjiezhang-z@users.noreply.github.com>
2025-07-01 14:02:20 +0800

    Update run.sh

--- train_scripts_cholec80/run.sh
+++ train_scripts_cholec80/run.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
 # Training
-python3 train.py phase --split cuhk4040 --bn_off --backbone convnextv2 --workers 4 --freeze --seq_len 256 --lr 1e-4 --random_seed --trial_name Step1 --cfg configs/cholec80.yaml
+python3 train.py phase --split cuhk4040 --bn_off --backbone convnextv2 --workers 4 --freeze --seq_len 256 --lr 1e-4 --random_seed --trial_name TRIAL_NAME --cfg configs/cholec80.yaml
 
 # Evaluation
 python3 save_predictions.py phase --split cuhk4040 --backbone convnextv2 --seq_len 256 --resume ../output/checkpoints/phase/[TRIAL_NAME]/models/checkpoint_best_acc.pth.tar --cfg configs/cholec80.yaml
